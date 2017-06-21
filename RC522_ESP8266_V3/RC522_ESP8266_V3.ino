27c11f2fa6889504e961fddcf8f456ac21c3d63e
wahxian <wxn13@ic.ac.uk>
2017-06-21 23:35:28 +0100

    Add files via upload

---
+++ RC522_ESP8266_V3/RC522_ESP8266_V3.ino
@@ -0,0 +1,323 @@
+/* Pin Layouts:
+  -------------------------------------------------------------------------------------------------
+              MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino      ESP8266
+              Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+  Signal      Pin          Pin           Pin       Pin        Pin              Pin
+  -------------------------------------------------------------------------------------------------
+  RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST          2
+  SPI SS      SDA(SS)      10            53        D10        10               10           4
+  SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16           13
+  SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14           12
+  SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15           14
+
+  MFRC522 Library was imported from: https://github.com/ljos/MFRC522
+*/
+
+#include <SPI.h>      // Used for communication via SPI with the Module
+#include <MFRC522B.h> // Include of the RC522 Library
+
+#include <NTPtimeESP.h>  // Include NTP Time Library to get NTP Time
+#include "ESP8266WiFi.h" //include wifi library
+#include <WiFiManager.h>  //Include library to enable WiFi Direct WiFi configuration
+#include <vector>;
+
+//========================NTP TIme Initialisation Variables==========================
+NTPtime NTPUK("uk.pool.ntp.org"); // Choose server pool as required
+strDateTime dateTime;
+String JSONdateTime;
+
+//========================RC522 Initialisation Variables==============================
+#define SDAPIN 4   // RFID Module SDA Pin is connected to the UNO 10 Pin
+#define RESETPIN 2 // RFID Module RST Pin is connected to the UNO 8 Pin
+
+byte foundTag;         // Variable used to check if Tag was found
+byte readTag;          // Variable used to store anti-collision value to read Tag information
+byte tagData[MAX_LEN]; // Variable used to store Full Tag Data
+byte TagID[4];         // Variable used to store only Tag Serial Number
+int matchingTagID;
+
+//---------------------Hard coded Items corresponding to tag ID------------------
+byte Chicken[4] = {0xB0, 0xD3, 0x8C, 0x50}; //Put 5??
+byte Cabbage[4] = {0x60, 0xA0, 0x83, 0x4D};
+byte Bread[4] = {0x82, 0xA4, 0x34, 0x5B};
+byte Milk[4] = {0x32, 0xB5, 0x34, 0x5B};
+
+struct Grocery
+{
+  int ProductId;
+  int UserId;
+  String ProductName;
+  int Weight;
+  float Price;
+  String Purchased;
+  String Expiry;
+  int Priority;
+  int AmountRemaining;
+  bool inFridge;
+};
+
+Grocery Database[4];
+
+MFRC522 hfRFID(SDAPIN, RESETPIN); // Init of the library using the UNO pins declared above
+
+//===========================FoodAppee Setup=====================
+const char *server = "foodappee.azurewebsites.net";
+WiFiClient client;
+const int postingInterval = 1 * 1000; // post data every 20 seconds
+String UserId = "7";
+String postData;
+
+//---------------------Functions-----------------------------------
+String zeroExtend(String time);
+void getDateTime();
+void initialiseData();
+int findTagID();
+void formatJSONString();
+void postJSONData();
+
+//------------------------------------------------------//
+//                          Setup                       //
+//------------------------------------------------------//
+void setup()
+{
+  //======================NTP and WiFi Setup=====================
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booted");
+  Serial.println("Connecting to Wi-Fi");
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Smart Fridge");
+
+  Serial.println("WiFi connected");
+
+  //===========================RC522 Setup=====================
+  SPI.begin();
+  Serial.begin(115200);
+  Serial.println("Looking for RFID Reader");
+  hfRFID.begin();
+  byte version = hfRFID.getFirmwareVersion(); // Variable to store Firmware version of the Module
+
+  // If can't find an RFID Module
+  if (!version)
+  {
+    Serial.print("Didn't find RC522 board.");
+    while (1)
+      ; //Wait until a RFID Module is found
+  }
+
+  // If found, print the information about the RFID Module
+  Serial.print("Found chip RC522 ");
+  Serial.print("Firmware version: 0x");
+  Serial.println(version, HEX);
+  Serial.println();
+
+  initialiseData();
+  Serial.println("Database is loaded into application");
+
+  Serial.println("The Scanned Item is: ");
+}
+
+//------------------------------------------------------//
+//                          Loop                        //
+//------------------------------------------------------//
+void loop()
+{
+
+  int itemTag = 0;
+
+  // ------------1. Check to see if a Tag was detected and print out serial number----------------
+  // If yes, then the variable foundTag will contain "MI_OK"
+  foundTag = hfRFID.requestTag(MF1_REQIDL, tagData); //request for tag and stores serial in tagData
+
+  if (foundTag == MI_OK)
+  {
+    //--------------------Get NTP Time for timestamp--------------------
+    getDateTime();
+
+    //---------------------------Getting Grocery RFID ID----------------------------
+    delay(100);
+
+    // Get anti-collision value to properly read information from the Tag using tagData(serial number)
+    readTag = hfRFID.antiCollision(tagData);
+
+    //SYNTAX: memcpy ( void * destination, const void * source, size_t num ) 4 bytes in this case
+    memcpy(TagID, tagData, 4); // Write the Tag information in the TagID array
+
+    //Store Tag data into Array to compare later
+    scannedTags.push_back(TagID);
+    
+    Serial.println("Tag detected.");
+    Serial.print("Tag ID of item:  ");
+    for (int i = 0; i < 4; i++)
+    { // Loop to print serial number to serial monitor
+      Serial.print(TagID[i], HEX); //print in hexadecimal format instead of bin/decimal
+      Serial.print(", ");
+    }
+    Serial.println();
+
+    //--------------------------Identifying Grocery by ID--------------------------
+    findTagID();
+    formatJSONString();
+    postJSONData();
+  }
+}
+
+//-----------------------------------------------------------//
+//                          Functions                        //
+//-----------------------------------------------------------//
+
+String zeroExtend(String time) {
+  if (time.length() == 1) {
+    return "0" + time;
+  } else {
+    return time;
+  }
+}
+
+void getDateTime() {
+  dateTime = NTPUK.getNTPtime(1.0, 0); //Get NTP time of UK
+  while (!dateTime.valid) {
+    Serial.println("failed to get NTP data...");
+    dateTime = NTPUK.getNTPtime(1.0, 0);
+    Serial.println("Retrying to get NTP data...");
+  }
+  Serial.println("Obtained NTP successfully");
+  NTPUK.printDateTime(dateTime);
+
+  byte iHour = dateTime.hour;
+  String actualHour = zeroExtend(String(iHour));
+  byte iMinute = dateTime.minute;
+  String actualMinute = zeroExtend(String(iMinute));
+  byte iSecond = dateTime.second;
+  String actualSecond = zeroExtend(String(iSecond));
+  int actualYear = dateTime.year;
+  byte actualMonth = dateTime.month;
+  byte actualDay = dateTime.day;
+  byte actualDayofWeek = dateTime.dayofWeek;
+
+  //  JSONdateTime = "\"" + String(dateTime.year) + "-" + String(dateTime.month) + "-" + String(dateTime.day) + "T" + String(dateTime.hour) + ":" + String(dateTime.minute) + ":" + String(dateTime.second) + "\"";
+  JSONdateTime = "\"" + String(actualYear) + "-" + String(actualMonth) + "-" + String(actualDay) + "T" + actualHour + ":" + actualMinute + ":" + actualSecond + "\"";
+  Serial.println("The NTP Date and Time is: " + JSONdateTime);
+
+}
+
+void initialiseData()
+{
+  //Chicken
+  Database[0].ProductId = 51;
+  Database[0].UserId = 7;
+  Database[0].ProductName = "Chicken";
+  Database[0].Weight = 500;
+  Database[0].Price = 4.60;
+  Database[0].Purchased = "";
+  Database[0].Expiry = "2017-06-20T09:12:46";
+  Database[0].Priority = 1;
+  Database[0].AmountRemaining = 200;
+  Database[0].inFridge = true;
+
+  //Cabbage
+  Database[1].ProductId = 52;
+  Database[1].UserId = 7;
+  Database[1].ProductName = "Cabbage";
+  Database[1].Weight = 200;
+  Database[1].Price = 4.60;
+  Database[1].Purchased = "";
+  Database[1].Expiry = "2017-06-16T09:12:46";
+  Database[1].Priority = 2;
+  Database[1].AmountRemaining = 100;
+  Database[1].inFridge = true;
+
+  //Bread
+  Database[2].ProductId = 53;
+  Database[2].UserId = 7;
+  Database[2].ProductName = "Bread";
+  Database[2].Weight = 600;
+  Database[2].Price = 4.60;
+  Database[2].Purchased = "";
+  Database[2].Expiry = "2017-06-14T09:12:46";
+  Database[2].Priority = 1;
+  Database[2].AmountRemaining = 100;
+  Database[2].inFridge = true;
+
+  //Milk
+  Database[3].ProductId = 54;
+  Database[3].UserId = 7;
+  Database[3].ProductName = "Milk";
+  Database[3].Weight = 1000;
+  Database[3].Price = 1.20;
+  Database[3].Purchased = "";
+  Database[3].Expiry = "2017-06-19T09:12:46";
+  Database[3].Priority = 1;
+  Database[3].AmountRemaining = 500;
+  Database[3].inFridge = true;
+}
+
+int findTagID()
+{
+  if (Chicken[0] == TagID[0] && Chicken[1] == TagID[1] && Chicken[2] == TagID[2] && Chicken[3] == TagID[3])
+  {
+    matchingTagID = 0;
+  }
+  else if (Cabbage[0] == TagID[0] && Cabbage[1] == TagID[1] && Cabbage[2] == TagID[2] && Cabbage[3] == TagID[3])
+  {
+    matchingTagID = 1;
+  }
+  else if (Bread[0] == TagID[0] && Bread[1] == TagID[1] && Bread[2] == TagID[2] && Bread[3] == TagID[3])
+  {
+    matchingTagID = 2;
+  }
+  else if (Milk[0] == TagID[0] && Milk[1] == TagID[1] && Milk[2] == TagID[2] && Milk[3] == TagID[3])
+  {
+    matchingTagID = 3;
+  }
+  else
+  {
+    Serial.print("Item does not exist");
+  }
+}
+
+void formatJSONString()
+{
+  //JSON Format
+  postData = "{\"UserId\":" + UserId + ",";
+  postData += "\"FoodItems\":[{";
+  postData += "\"ProductId\":" + String(Database[matchingTagID].ProductId) + ",";
+  postData += "\"UserId\":" + String(Database[matchingTagID].UserId) + ",";
+  postData += "\"ProductName\":\"" + String(Database[matchingTagID].ProductName) + "\",";
+  postData += "\"Weight\":" + String(Database[matchingTagID].Weight) + ",";
+  postData += "\"Price\":" + String(Database[matchingTagID].Price) + ",";
+  postData += "\"Purchased\":" + JSONdateTime + ",";
+  postData += "\"Expiry\":\"" + String(Database[matchingTagID].Expiry) + "\",";
+  postData += "\"Priority\":" + String(Database[matchingTagID].Priority) + ",";
+  postData += "\"AmountRemaining\":" + String(Database[matchingTagID].AmountRemaining) + ",";
+  postData += "\"inFridge\":" + String(Database[matchingTagID].inFridge) + "}]}";
+  Serial.println(postData);
+}
+
+void postJSONData()
+{
+  if (client.connect(server, 80))
+  {
+    Serial.println("Sending data to Database");
+    //................Format that FoodAppee uses...................
+    client.println("POST /AddItems HTTP/1.1");
+    client.println("Host: foodappee.azurewebsites.net");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json"); //application/json
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println("\n\n");
+    client.println(postData);
+    client.println("\n\n");
+  }
+  else
+  {
+    Serial.println("Connection Failed");
+  }
+  Serial.println("Data Posted up to database");
+  Serial.println();
+  Serial.println();
+  client.stop(); //Stop connecting to Thingspeak server
+}
+
---
+++ smart_dustbin_V5/smart_dustbin_V5.ino
@@ -0,0 +1,286 @@
+#include "HX711.h"
+#include "ESP8266WiFi.h"
+#include <NTPtimeESP.h> //Include NTP library to get NTP time
+#include <WiFiManager.h>
+//Library for OLED
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+
+//--------NTP Time------------------------
+#define DEBUG_ON
+NTPtime NTPUK("uk.pool.ntp.org"); // Choose server pool as from UK
+strDateTime dateTime;   //Structure to store date and time info
+String JSONdateTime;
+
+//--------WiFi Sleep-------------
+const int sleepTimeSeconds = 24 * 60 * 60;
+
+//--------Ultrasonic Range Sensor-------------
+#define TRIGGER 13
+#define ECHO 15
+float duration, distance, volume;
+
+//------Custom Dustbin Dimensions-----
+char confLength[20] = "";
+char confWidth[20] = "";
+char confHeight[20] = "";
+
+//--------Load Cell---------------------------
+HX711 scale;
+float weight;
+
+//-----FoodAppee API Settings-----
+WiFiClient client;                     //Create client to connect to specified IP and port defined in .connect()
+const int postingInterval = 10 * 1000; //Post data every 10 seconds
+const char *server = "foodappee.azurewebsites.net";
+String UserId = "7";
+String postData;
+
+//----------OLED----------
+SSD1306  display(0x3c, 4, 14);
+String testing = "OK";
+
+//---------RGB LED--------------
+int redPin = 1;
+int greenPin = 3;
+int bluePin = 12;
+
+//uncomment this line if using a Common Anode LED
+//#define COMMON_ANODE
+
+//-----Functions-----
+String zeroExtend(String time);
+void getDateTime();
+void printMetrics();
+void postJSONData();
+void formatJSONString();
+
+//###########################################
+//               SETUP                      #
+//###########################################
+void setup()
+{
+
+  //--------Ultrasonic Range Sensor-------------
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
+
+  // Wait for serial to initialize.
+  while (!Serial) { }
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(0, OUTPUT);
+
+  //-----RGB LED-----
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  //--------Load Cell---------------------------
+  Serial.println("Initializing the scale"); // parameter "gain" is ommited; the default value 128 is used by the library
+  scale.begin(0, 2);                       //DOUT,SCK
+
+  scale.set_scale(-455.f); // this value is obtained by calibrating the scale with known weights;
+  scale.tare();
+
+  //---------------------Setup WiFi and Serial-----------------------------------------
+  WiFiManager wifiManager;
+
+  // Adding an additional config on the WIFI manager webpage for the API Key and Channel ID
+  WiFiManagerParameter customLenght("confLength", "Length", confLength, 20);
+  WiFiManagerParameter customWidth("confWidth", "Width", confWidth, 20);
+  WiFiManagerParameter customHeight("confHeight", "Height", confHeight, 20);
+  wifiManager.addParameter(&customLenght);
+  wifiManager.addParameter(&customWidth);
+  wifiManager.addParameter(&customHeight);
+
+  wifiManager.autoConnect("Smart Dustbin");
+
+  strcpy(confLength, customLenght.getValue());
+  strcpy(confWidth, customWidth.getValue());
+  strcpy(confHeight, customHeight.getValue());
+
+  Serial.println("The configurations are: ");
+  //  if (confLength!=NULL) {
+  //    Serial.println(String(confLength) + "\t" + String(confWidth) + "\t" + String(confHeight));
+  //  }
+
+  //-----------------------------------------------------------------------------------------------------
+  //---------------------Get NTP Time when starting up---------------------
+  getDateTime();
+
+  //--------Ultrasonic Range Sensor-------------
+  digitalWrite(TRIGGER, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  Serial.print(distance);   //Print distance in cm
+  if (distance <= 5 ) {
+    setColor(255, 0, 0);  // red
+  } else {
+    setColor(0, 0, 255);  // blue
+  }
+  Serial.println("Centimeter:");
+
+  volume = 25 * 40 * (50 - distance);
+  delay(1000);
+
+  //--------Load Cell---------------------------
+  Serial.print("one reading:\t");
+  Serial.print(scale.get_units(), 2);
+  Serial.print("\t| average:\t");
+  weight = scale.get_units(10);
+  Serial.println(weight, 2);
+
+  scale.power_down(); // put the ADC in sleep mode
+  delay(1000);
+  scale.power_up();
+
+  //-------OLED Initialisation-------
+  display.init();
+  display.setContrast(255);
+  printMetrics();
+
+  //----------POST data to server----------
+  formatJSONString();
+  postJSONData();
+
+  //--------------------------------------------------------------------------------------------------------------
+
+  Serial.println("ESP8266 Going into Deep Slumber for: " + String(sleepTimeSeconds) + "Seconds");
+  ESP.deepSleep(sleepTimeSeconds * 1000000);    //GPIO16 to RST pin for this to work
+//  delay(5000);
+
+}
+
+//###########################################
+//                LOOP                      #
+//###########################################
+
+void loop()
+{
+
+}
+
+//###########################################
+//                Functions                 #
+//###########################################
+
+String zeroExtend(String time) {
+  if (time.length() == 1) {
+    return "0" + time;
+  } else {
+    return time;
+  }
+}
+
+void getDateTime() {
+  strDateTime dateTime;   //Structure to store date and time info
+  Serial.println("Getting NTP Data");
+  dateTime = NTPUK.getNTPtime(1.0, 0);
+  while (!dateTime.valid) {
+    Serial.print("Error reading NTP data");
+    delay(100);
+    dateTime = NTPUK.getNTPtime(1.0, 0);
+  }
+  Serial.println();
+  byte iHour = dateTime.hour;
+  String actualHour = zeroExtend(String(iHour));
+  byte iMinute = dateTime.minute;
+  String actualMinute = zeroExtend(String(iMinute));
+  byte iSecond = dateTime.second;
+  String actualSecond = zeroExtend(String(iSecond));
+  int actualYear = dateTime.year;
+  byte actualMonth = dateTime.month;
+  byte actualDay = dateTime.day;
+  byte actualDayofWeek = dateTime.dayofWeek;
+
+  //  JSONdateTime = "\"" + String(dateTime.year) + "-" + String(dateTime.month) + "-" + String(dateTime.day) + "T" + String(dateTime.hour) + ":" + String(dateTime.minute) + ":" + String(dateTime.second) + "\"";
+  JSONdateTime = "\"" + String(actualYear) + "-" + String(actualMonth) + "-" + String(actualDay) + "T" + actualHour + ":" + actualMinute + ":" + actualSecond + "\"";
+  Serial.println(JSONdateTime);
+
+}
+
+void printMetrics() {
+  // Initialize the log buffer
+  // allocate memory to store 8 lines of text and 30 chars per line.
+  display.setLogBuffer(5, 30);
+  String tmpWeight = String(weight) + "g";
+  String tmpVolume = String(volume) + "cm3";
+
+  // Some test data
+  const char* test[] = {
+    JSONdateTime.c_str(), "Weight: ", tmpWeight.c_str(), "Volume: ", tmpVolume.c_str()
+  };
+  //Use .c_str to convert strings to char arrays
+
+  for (uint8_t i = 0; i < 5; i++) {
+    display.clear();
+    // Print to the screen
+    display.println(test[i]);
+    // Draw it to the internal screen buffer
+    display.drawLogBuffer(0, 0);
+    // Display it on the screen
+    display.display();
+    delay(500);
+  }
+}
+
+
+void formatJSONString()
+{
+  //JSON Format
+  postData = "{\"UserId\":" + UserId + ",";
+  postData += "\"Timestamp\":" + JSONdateTime + ",";
+  postData += "\"Weight\":" + String(weight) + "}";
+  Serial.println(postData);
+}
+
+void postJSONData()
+{
+  //----------FoodAppee API POST Data-----
+  if (client.connect(server, 80))
+  {
+    client.println("POST /postWaste HTTP/1.1");
+    client.println("Host: foodappee.azurewebsites.net");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.print(postData.length());
+    client.print("\n\n");
+    client.println(postData);
+    client.print("\n\n");
+
+    Serial.println("POST /postWaste HTTP/1.1");
+    Serial.println("Host: foodappee.azurewebsites.net");
+    Serial.println("Content-Type: application/json");
+    Serial.print("Content-Length: ");
+    Serial.print(postData.length());
+    Serial.print("\n\n");
+    Serial.println(postData);
+    Serial.print("\n\n");
+  }
+  client.stop();          //Stop connecting to Thingspeak server
+  delay(postingInterval); // wait and then post again
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
